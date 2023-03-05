a54d372a9b807e61578214cdb398ac7bdbc91397
Darsing <catalunasiegfried@gmail.com>
2023-03-05 23:35:13 +0000

    updated contact info

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import Learn from './Learn';
+import { Link} from 'react-router-dom';
+
 
 function Contact(props) {
   const contactImage = require(`../../images/contact.png`);
@@ -8,27 +8,23 @@ function Contact(props) {
   return (
     <div style = {{backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.4) 0%,rgba(255,255,255,0.9) 100%) , url(${bgImage})`}}>
       <div className="card mx-auto " style={{ width: "18rem" }}>
-        <img src={contactImage} className="card-img-top" alt="contact-me-photo" />
+        <img src={contactImage} className="card-img-top" alt="contact-me" />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 className="card-title">Contact Information:</h5>
+          <p className="card-text">You can reach me via telephone, email or check out my github page to see more of my work.</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">+447572295418</li>
+          <li className="list-group-item">catalunasiegfried@gmail.com</li>
         </ul>
         <div className="card-body">
-          <Link to="learn" role="button" classNameName="btn btn-primary">
+          <Link to="https://github.com/darylcata" role="button" classNameName="btn btn-primary">
             Github Account
           </Link>
           &nbsp;
-          <Link to="contact" role="button" classNameName="btn btn-primary">
+          <Link to="https://www.linkedin.com/in/siegfried-daryl-catalu%C3%B1a-a51891138" role="button" classNameName="btn btn-primary">
             LinkedIn
           </Link>
-          <Routes>
-            <Route path="learn" element={<Learn />} />
-          </Routes>
         </div>
       </div>
     </div>
