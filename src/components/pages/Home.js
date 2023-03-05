36148818f97460dafcf9b16a6e6c253296bc7c99
Darsing <catalunasiegfried@gmail.com>
2023-03-05 22:32:19 +0000

    added home page bg image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 function Home() {
+  const bgImage = require(`../../images/bgImage.png`)
   return (
-    <header style={{
-      backgroundColor: "red", 
-      height: "400px"
+    <header className="header" style={{
+      backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.3) 0%,rgba(255,255,255,0.6) 100%) , url(${bgImage})`,
+      height: "500px"
     }}>
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
-          <h1 className="display-4" style={{ margin: "0", paddingTop: "75px", textAlign: "center", color: "white", fontSize: 100 }}>Welcome</h1>
-          <p className="lead">This is a simple portfolio about me, my work and skills I hope to achieve in this bootcamp. I am
-            new to this industry and very interested.</p>
+          <h1 className="display-4" style={{ margin: "0", paddingTop: "100px", textAlign: "center", fontSize: 125 }}>Welcome</h1>
+          <p className="lead"><strong>This is a simple portfolio about me, my work and skills I hope to achieve in this bootcamp. I am
+            new to this industry and very interested.</strong></p>
+          <p className="lead"><strong>Have a look around my recent project through the Navbar or if you fancy, contact me through below.</strong></p>
         </div>
       </div>
     </header>
