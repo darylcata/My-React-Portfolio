5aad5fe06666bd6474a04c24d82258b48fb4a1a5
Darsing <catalunasiegfried@gmail.com>
2023-03-08 22:30:18 +0000

    added form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function Contact(props) {
@@ -8,7 +8,7 @@ function Contact(props) {
   const myCv = require(`../../images/daryl-cv.pdf`)
 
   return (
-    <div style = {{backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.4) 0%,rgba(255,255,255,0.9) 100%) , url(${bgImage})`}}>
+    <div style={{ display: "flex", backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.4) 0%,rgba(255,255,255,0.9) 100%) , url(${bgImage})` }}>
       <div className="card mx-auto " style={{ width: "18rem" }}>
         <img src={contactImage} className="card-img-top" alt="contact-me" />
         <div className="card-body">
@@ -18,18 +18,33 @@ function Contact(props) {
         <ul className="list-group list-group-flush">
           <li className="list-group-item">+447572295418</li>
           <li className="list-group-item">catalunasiegfried@gmail.com</li>
-          <a className="btn btn-secondary" href={myCv}>**Here is my CV!**</a>
+          <a className="btn btn-primary" href={myCv}>**Here is my CV!**</a>
         </ul>
         <div className="card-body">
-          <Link to="https://github.com/darylcata" role="button" classNameName="btn btn-primary">
-            Github account
+          <Link to="https://github.com/darylcata" role="button" className='btn btn-primary'>
+            to: Github account
           </Link>
-          &nbsp;
-          <Link to="https://www.linkedin.com/in/siegfried-daryl-catalu%C3%B1a-a51891138" role="button" classNameName="btn btn-primary">
-            LinkedIn
+          <Link to="https://www.linkedin.com/in/siegfried-daryl-catalu%C3%B1a-a51891138" role="button" className='btn btn-primary'>
+            to: LinkedIn
           </Link>
         </div>
       </div>
+      <form className="card mx-auto " style={{ width: "20rem" }}>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
